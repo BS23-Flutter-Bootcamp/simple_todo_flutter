f8f65bc35ec0e689d775c6c89627188affb5ba43
Muhammad Saiful Islam <si0173520@gmail.com>
2025-04-21 12:28:05 +0600

    Add provider to the main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:simple_todo_flutter/view/splash_screen.dart';
+import 'package:simple_todo_flutter/view_model/task_viewmodel.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => TaskViewModel(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
